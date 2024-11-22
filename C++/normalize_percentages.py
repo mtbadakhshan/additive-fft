66817bb4e44f64ccf589576c32b64aa30d6b18d3
Taghi <mtbadakhshan@gmail.com>
2024-11-21 19:55:55 -0500

    minor update

--- C++/normalize_percentages.py
+++ C++/normalize_percentages.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import csv
 
-file_path = './build/chrono_output.csv'  # Assuming the file is named 'data.csv' and is uploaded
+path_root = '../data_backup/chrono_timer/'
+file_path = path_root + 'chrono_output.csv' 
 data = pd.read_csv(file_path)
 
 normalized_data = data.iloc[:, 1:].div(data.iloc[:, 1:].sum(axis=1), axis=0) 
 normalized_data.insert(0, 'm', data['m'])
 
-output_file_path = './build/normalized_chrono_output.csv'
+output_file_path = path_root + 'normalized_chrono_output.csv'
 normalized_data.to_csv(output_file_path, index=False)
