a7bbc58290adf4178d4c7abe7e78c6c1ed62ff9d
Taghi <mtbadakhshan@gmail.com>
2025-01-08 10:57:12 -0500

    Finish LCH testing and start the Cantor FFT

--- C/main.c
+++ C/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <emmintrin.h>
+#include <time.h>
+
 #include <immintrin.h>
+
 #include "utils/lch_api.h"
 #include "utils/utils.h"
 #include "bitpolymul/bc.h"
@@ -10,31 +12,24 @@
 
 #define LOG2(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
 
-__m128i* random_polynomial_gf2128(unsigned n){
-    __m128i* fx = (__m128i*)aligned_alloc( 32 , sizeof(__m128i)*n );
-	if( NULL == fx ) { printf("alloc fail.\n"); exit(-1); }
-	for (unsigned i = 0; i < n; ++i) fx[i] = _mm_set_epi32(rand(), rand(), rand(), rand());
-    return fx;
-} 
-
 void test_bitpolymul_lch(){
-	unsigned m = 3; // degree n = 2^m
+	unsigned m = 5; // degree n = 2^m
 	unsigned n = (1ULL) << m; // n is even and it must be even for keeping the 32 byte
-
+    printf("m = %u, n = %u\n", m, n);
     // Generating the random polynomial in GF_2^128
 	__m128i* fx = random_polynomial_gf2128(n);
-    print_vector_128bit_name(fx, n, "fx");
     __m128i* evals = lch_fft_gf2128(fx, n);
-    print_vector_128bit_name(fx, n, "fx");
+    __m128i* evals2 = naive_evaluate(fx, n);
+    printf("are equal = %b\n", are_equal_vec_128bit(evals, evals2, n));
+}
 
+void test_cantor(){
     
-    // _bc_to_lch_128( fx , n , 1 );
-    // butterfly_net_clmul( fx ,n );
-
-
 }
 
 int main(){
+    srand(time(NULL));
+    // validate_cantor_basis();
     test_bitpolymul_lch();
     return 0;
 }
