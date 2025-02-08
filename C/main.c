a6dc02345efe11f1ae2a745e558711b3ef2e5dbd
Taghi <mtbadakhshan@gmail.com>
2025-02-07 20:05:24 -0500

    Hard core the division circuits in Cantor

--- C/main.c
+++ C/main.c
@@ -25,7 +25,7 @@ void test_bitpolymul_lch(){
 }
 
 void test_cantor(){
-    unsigned m = 10; // degree n = 2^m
+    unsigned m = 4; // degree n = 2^m
 	unsigned n = (1ULL) << m; // n is even and it must be even for keeping the 32 byte
     printf("m = %u, n = %u\n", m, n);
     // Generating the random polynomial in GF_2^128
@@ -35,31 +35,57 @@ void test_cantor(){
     printf("are equal = %b\n", are_equal_vec_128bit(evals, evals2, n));
 }
 
-#define ITERATIONS 10
+#define ITERATIONS 100
 void cantor_vs_lch(){
-    printf("m\tCantor\t\tLCH\n");
-    for (unsigned m = 3; m < 22; m++){
+    printf("m\tCantor\t\tCantor HC\tCantor CACHE\tLCH\n");
+    for (unsigned m = 5; m < 21; m++){
 	    unsigned n = (1ULL) << m; 
         clock_t start, end;
-        double time_cantor = 0, time_lch = 0;
+        double time_cantor = 0, time_cantor_hc = 0, time_lch = 0, time_cantor_hc_cache=0;
+        
+        for (unsigned iter = 0; iter < ITERATIONS; ++iter){
+            __m128i* fx1 = random_polynomial_gf2128(n);
+            start = clock();
+            __m128i* evals = cantor_fft_gf2128(fx1, n);
+            end = clock();
+            time_cantor += 1000 * ((double) (end - start)) / CLOCKS_PER_SEC;
+            free(fx1);
+            free(evals);
+        }
 
         for (unsigned iter = 0; iter < ITERATIONS; ++iter){
             __m128i* fx1 = random_polynomial_gf2128(n);
             start = clock();
-            __m128i* evals2 = lch_fft_gf2128(fx1, n);
+            __m128i* evals = cantor_fft_hc_cache_gf2128(fx1, n);
+            end = clock();
+            time_cantor_hc_cache += 1000 * ((double) (end - start)) / CLOCKS_PER_SEC;
+            free(fx1);
+            free(evals);
+        }
+
+        for (unsigned iter = 0; iter < ITERATIONS; ++iter){
+            __m128i* fx1 = random_polynomial_gf2128(n);
+            start = clock();
+            __m128i* evals = cantor_fft_hc_circuits_gf2128(fx1, n);
+            end = clock();
+            time_cantor_hc += 1000 * ((double) (end - start)) / CLOCKS_PER_SEC;
+            free(fx1);
+            free(evals);
+        }
+
+        for (unsigned iter = 0; iter < ITERATIONS; ++iter){
+            __m128i* fx1 = random_polynomial_gf2128(n);
+            start = clock();
+            __m128i* evals = lch_fft_gf2128(fx1, n);
             end = clock();
             time_lch += 1000 * ((double) (end - start)) / CLOCKS_PER_SEC;
-
-            start = clock();
-            __m128i* evals1 = cantor_fft_gf2128(fx1, n);
-            end = clock();
-            time_cantor += 1000 * ((double) (end - start)) / CLOCKS_PER_SEC;
-
-            // printf("are equal = %b\n", are_equal_vec_128bit(evals1, evals2, n));
-        }   
+            free(fx1);
+            free(evals);
+        }
+    
         // __m128i* fx2 = random_polynomial_gf2128(n);
 
-        printf("%u\t%fms\t%fms\n", m, time_cantor/ITERATIONS, time_lch/ITERATIONS);
+        printf("%u\t%f ms\t%f ms\t%f ms\t%f ms\n", m, time_cantor/ITERATIONS, time_cantor_hc/ITERATIONS, time_cantor_hc_cache/ITERATIONS, time_lch/ITERATIONS);
     }
 }
 
